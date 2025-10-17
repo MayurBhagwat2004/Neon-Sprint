04bba1441c1da463964eee5803124f4afdc8e765
MayurBhagwat2004 <mayurbhagwat100@gmail.com>
2025-10-17 20:37:31 +0530

    Added some changes in gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+    private bool playerTouchedScreen;
+    
     void Start()
     {
         
