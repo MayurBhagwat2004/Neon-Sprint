dcb0a333ef9ac480e739e9c1f128a89306c41815
MayurBhagwat2004 <mayurbhagwat100@gmail.com>
2025-10-17 13:26:40 +0530

    Modified sound logic

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -11,12 +11,19 @@ public class AudioManager : MonoBehaviour
     public AudioSource musicSource;
     public AudioSource sfxSource;
     public AudioClip[] soundClips;
+    public bool isMusicOn;
 
     void Awake()
     {
         if (Instance != this && Instance != null) Destroy(gameObject);
         else Instance = this;
         DontDestroyOnLoad(this);
+
+        musicSource = transform.GetChild(0).GetComponent<AudioSource>(); //Getting the AudioSource component from first child
+        sfxSource = transform.GetChild(1).GetComponent<AudioSource>(); // Getting the AudioSource component from second child
+
+        isMusicOn = PlayerPrefs.GetInt("MusicEnabled", 1) == 1; //Setting the MusicEnabled playerpref variable to 1 by default to play music
+
     }
 
     private void OnEnable()
@@ -30,8 +37,6 @@ public class AudioManager : MonoBehaviour
     }
     void Start()
     {
-        musicSource = transform.GetChild(0).GetComponent<AudioSource>(); //Getting the AudioSource component from first child
-        sfxSource = transform.GetChild(1).GetComponent<AudioSource>(); // Getting the AudioSource component from second child
     
     
     }
@@ -44,19 +49,34 @@ public class AudioManager : MonoBehaviour
     // Music controlling functions
     public void ChangeMusic(Scene scene, LoadSceneMode loadSceneMode)
     {
+        if (!isMusicOn) return; //Checking if the isMusicOn is 1 or else return
+
         string sceneName = scene.name;
 
         if (sceneName == "Home")
         {
             musicSource.clip = Array.Find(soundClips, s => s.name == "HomeMusic"); //Finding if there is any clip name HomeMusic to load in musicSource
         }
+        AudioClip audioClip = Array.Find(soundClips, clip => clip.name == "ClickSfx"); //Finding if there is any clip name ClickSfc to laod in sfxSource        
 
         musicSource.Play();
+    }
+
+    public void PlayMusic()
+    {
+        PlayerPrefs.SetInt("MusicEnabled",1);
+        if (musicSource != null)
+            musicSource.Play();
+    }
+    public void PauseMusic()
+    {
+        PlayerPrefs.SetInt("MusicEnabled",0);
+        if (musicSource != null)
+            musicSource.Pause();
     }
     
     public void PlayClickSound()
     {
-        AudioClip audioClip = Array.Find(soundClips,clip => clip.name == "ButtonClickSfx");        
-        musicSource.PlayOneShot(audioClip);
+        sfxSource.Play();
     }
 }
