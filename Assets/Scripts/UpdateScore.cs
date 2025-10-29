5e5a43949099e82a87e377f9b73a5a8b6dcbe249
MayurBhagwat2004 <mayurbhagwat100@gmail.com>
2025-10-29 18:49:31 +0530

    Removed home scene

---
+++ Assets/Scripts/UpdateScore.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UpdateScore : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    void Start()
+    {
+        if (scoreText != null)
+            scoreText.text = PlayerPrefs.GetInt("Score").ToString();
+    }
+
+    void Update()
+    {
+        
+    }
+}
