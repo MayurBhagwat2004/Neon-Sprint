69d7964434c537bf73cb2a63b6f7f0c0b7d03a76
MayurBhagwat2004 <mayurbhagwat100@gmail.com>
2025-10-29 19:25:54 +0530

    Modified sound logic

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
@@ -12,11 +13,12 @@ public class AudioManager : MonoBehaviour
     public AudioSource sfxSource;
     public AudioClip[] soundClips;
     public bool isMusicOn;
+    public Sprite[] soundButtonsrcImages;
+    public Button soundButton;
     void Awake()
     {
         if (Instance != this && Instance != null) Destroy(gameObject);
         else Instance = this;
-        DontDestroyOnLoad(this);
 
         musicSource = transform.GetChild(0).GetComponent<AudioSource>(); //Getting the AudioSource component from first child
         sfxSource = transform.GetChild(1).GetComponent<AudioSource>(); // Getting the AudioSource component from second child
@@ -26,20 +28,27 @@ public class AudioManager : MonoBehaviour
 
     public void HandleMusic()
     {
-        if (PlayerPrefs.GetInt("MusicEnabled") == 0) PauseMusic();
+        if (PlayerPrefs.GetInt("MusicEnabled") == 1) PauseMusic();
         else PlayMusic();
     }
     private void PlayMusic()
     {
         PlayerPrefs.SetInt("MusicEnabled", 1);
         if (musicSource != null)
+        {
             musicSource.Play();
+            soundButton.image.sprite = soundButtonsrcImages[0];
+
+        }
     }
     private void PauseMusic()
     {
         PlayerPrefs.SetInt("MusicEnabled", 0);
         if (musicSource != null)
+        {
             musicSource.Pause();
+            soundButton.image.sprite = soundButtonsrcImages[1];
+        }
     }
 
 
