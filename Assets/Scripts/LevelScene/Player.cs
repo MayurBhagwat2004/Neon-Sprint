b11986cdcd5421bdc21695555ee1e88226c1c96e
MayurBhagwat2004 <mayurbhagwat100@gmail.com>
2026-04-10 19:46:08 +0530

    Add the glow effect

--- Assets/Scripts/LevelScene/Player.cs
+++ Assets/Scripts/LevelScene/Player.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using System;
 public class Player : MonoBehaviour
 {
-    [SerializeField] private bool amIAlive;
+    public bool canMove;
     private bool superSonicAbilityEnabled;
     private Camera mainCamera;
     //Touch inputs variables
     public Vector2 touchPos;
-
+    private bool isDragging;
+    private float moveSpeed = 15f;
+    [SerializeField]private float maxY = 3.5f;
+    [SerializeField] private float minY = -4.5f;
     void Awake()
     {
         mainCamera = Camera.main;
@@ -15,23 +20,38 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        amIAlive = true;
+        if(GameManager.Instance!=null && GameManager.Instance.isGamePaused != true)
         superSonicAbilityEnabled = false;
 
     }
 
     void Update()
     {
-        if(!amIAlive) return;
+        canMove = !GameManager.Instance.isGamePaused;
 
-        if(Pointer.current != null && Pointer.current.press.isPressed)
+        if(!canMove) return;
+
+
+        if(Pointer.current!=null)
         {
-            touchPos = Pointer.current.position.ReadValue();
+            if (Pointer.current.press.wasPressedThisFrame)
+            {
+                if(EventSystem.current.IsPointerOverGameObject()) isDragging = false; //Checking if the player clicks on the ui so that the dragging of the neon ball stops
+                else isDragging = true;
+            }
 
-            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPos);
-            worldPosition.z = transform.position.z;
+            if(Pointer.current.press.wasReleasedThisFrame) isDragging = false;
 
-            transform.position = worldPosition;
+            if(Pointer.current.press.isPressed && isDragging)
+            {
+                touchPos = Pointer.current.position.ReadValue();
+
+                Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPos);
+                worldPosition.z = transform.position.z;
+                worldPosition.y = Mathf.Clamp(worldPosition.y,minY,maxY);
+
+                transform.position = Vector3.Lerp(transform.position,worldPosition,moveSpeed * Time.deltaTime);
+            }
         }    
     }
 }
