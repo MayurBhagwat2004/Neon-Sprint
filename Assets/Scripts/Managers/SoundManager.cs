da6ddb7d9b2e0193a8c2d04d7ad595216b5252d9
MayurBhagwat2004 <mayurbhagwat100@gmail.com>
2026-05-06 21:08:45 +0530

    Add The Fix For Volume Slider Level

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -15,7 +15,9 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private List<AudioClip> musicClips;
     [SerializeField] private List<AudioClip> sfxClips;
 
-    private const string SOUND_KEY = "SoundStatus";
+    private const string SOUND_VOLUME_KEY = "SoundVolume"; //Player Pref key for fetching the sound volume
+    private const string SOUND_KEY = "SoundStatus"; //Player Pref key for fetching the status of sound as 0/1 (off/on)
+    private float soundVolume;
     private int soundStatus; //Variable to keep track of the music
 
     public Slider volumeSlider;
@@ -36,6 +38,8 @@ public class SoundManager : MonoBehaviour
         {
             PlayerPrefs.SetInt(SOUND_KEY,1);
         }
+
+        
     }
 
     void Start()
@@ -65,7 +69,7 @@ public class SoundManager : MonoBehaviour
 
     void OnSceneLoaded(Scene scene,LoadSceneMode loadSceneMode)
     {
-        if(PlayerPrefs.GetInt("SoundStatus") == 1) //Checking if the sound is enabled or not
+        if(PlayerPrefs.GetInt(SOUND_KEY) == 1) //Checking if the sound is enabled or not
         {
             if(scene.name == SceneNames.Home.ToString())
             {
@@ -76,6 +80,12 @@ public class SoundManager : MonoBehaviour
                 if(volumeSlider == null)
                 {
                     volumeSlider = GameObject.Find("VolumeSlider").GetComponent<Slider>();
+
+                    if (PlayerPrefs.HasKey(SOUND_VOLUME_KEY))
+                    {
+                        volumeSlider.value = PlayerPrefs.GetFloat(SOUND_VOLUME_KEY,musicSource.volume); //Set the volume slider value as per the playerpref settings
+                        PlayerPrefs.Save();
+                    }
                 }
             }
             else if(scene.name == SceneNames.Level.ToString())
@@ -101,8 +111,10 @@ public class SoundManager : MonoBehaviour
             {
                 PlayerPrefs.SetInt(SOUND_KEY,1);
                 soundStatus = 1;
-
             }
+
+            PlayerPrefs.SetFloat(SOUND_VOLUME_KEY,musicSource.volume);
+
         }
         else
         {
